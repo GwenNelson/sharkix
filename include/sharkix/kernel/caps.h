fb5410efe52f165d74f5d0b373de9c3b428fd7bc
Gwen Nelson <gwen@gwennelson.co.uk>
2026-09-06 02:23:22 +0100

    fleshed out full caps system header

--- include/sharkix/kernel/caps.h
+++ include/sharkix/kernel/caps.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #include <libfifo/sync.h>
 
@@ -13,6 +14,9 @@ typedef uint64_t cap_handle_t;
 // handle for a generic kernel object
 typedef uint64_t kobject_handle_t;
 
+// handle for a capset
+typedef uint64_t capset_handle_t;
+
 // a bitmask/bitmap for rights granted to a cap
 typedef uint64_t cap_rights_t;
 
@@ -21,6 +25,11 @@ typedef uint64_t cap_rights_t;
 #define CAP_RIGHT_NONE		 UINT64_C(0)	    /* No rights at all                      */
 #define CAP_RIGHT_DELEGATE	(UINT64_C(1) << 0)  /* Can delegate this cap to another task */
 #define CAP_RIGHT_DESTROY	(UINT64_C(1) << 1)  /* Can destroy the underlying object     */
+
+// mask defining all valid rights for any generic object
+// this should be updated if any reserved bits get used
+#define CAP_GENERIC_VALID_RIGHTS	(CAP_RIGHT_DELEGATE | \
+					 CAP_RIGHT_DESTROY)
 
 // reserved for future standard perms
 #define CAP_RIGHT_RESV2		(UINT64_C(1) << 2)
@@ -36,6 +45,12 @@ typedef uint64_t cap_rights_t;
 #define CAP_RIGHT_IPC_SEND	(UINT64_C(1) << 10)
 #define CAP_RIGHT_IPC_CALL	(UINT64_C(1) << 11)
 #define CAP_RIGHT_IPC_RECV	(UINT64_C(1) << 12)
+
+// all rights that make sense for an IPC endpoint
+#define CAP_IPC_VALID_RIGHTS	(CAP_GENERIC_VALID_RIGHTS | \
+				 CAP_RIGHT_IPC_SEND | \
+				 CAP_RIGHT_IPC_CALL | \
+				 CAP_RIGHT_IPC_RECV)
 
 // helpers
 #define CAP_HAS_ALL(cap, required) \
@@ -87,7 +102,51 @@ typedef struct cap_t {
 // represents a set of caps, such as those held by a particular task
 typedef struct capset_t {
 	capset_handle_t	capset_handle;  // handle for this set
-	cap_t*		caps;		// the actual caps inside it
+	cap_handle_t*	cap_handles;    // the actual caps inside it, or at least their handles
 	fifo_spinlock_t spinlock;	// duh
 	UT_hash_handle  hh;		// duh
 } capset_t;
+
+// setup the global caps table
+void kinit_caps(void);
+
+// create a new cap
+int  kcap_create(kobject_handle_t, cap_rights_t init_rights, cap_handle_t* new_cap);
+
+// destroy a cap - this is NOT the same thing as destroying the underlying object, which must be implemented by the underlying subsystem
+int  kcap_destroy(cap_handle_t cap);
+
+// derive a new cap from an existing one with only the specified subset of rights
+int  kcap_derive(cap_handle_t source, cap_rights_t new_rights, cap_handle_t *new_cap);
+
+// derive a new cap from an existing one while copying over ALL rights
+int  kcap_clone(cap_handle_t source, cap_handle_t *new_cap);
+
+// create a new cap from two old ones, assuming both point to the same underlying object
+// this is non-destructive, the two old caps continue to be valid unless destroyed
+int  kcap_merge(cap_handle_t a, cap_handle_t b, cap_handle_t *new_cap);
+
+// check if a cap exists in the global caps table
+bool kcap_cap_exists(cap_handle_t cap);
+
+// try and get the actual cap itself from the global table - or rather, a pointer to it
+int kcap_getcap(cap_handle_t handle, cap_t **cap);
+
+// allocate and create a new capset
+int  kcapset_new(capset_handle_t *new_set);
+
+// add a cap to a capset
+int  kcapset_addcap(capset_handle_t set, cap_handle_t cap);
+
+// delete a cap from a capset - the cap remains globally valid!
+int  kcapset_delcap(capset_handle_t set, cap_handle_t cap);
+
+// check if a capset has a particular cap
+bool kcapset_hascap(capset_handle_t set, cap_handle_t cap);
+
+// check if a capset has any caps of a particular type with particular rights
+// if so, return it in *out
+int kcapset_resolve_cap(capset_handle_t set, cap_type_t req_type, cap_rights_t req_rights, cap_t *out);
+
+// check if a capset has any caps allowing the specified operations on a particular type, without actually obtaining it
+int kcapset_check_perms(capset_handle_t set, cap_type_t req_type, cap_rights_t req_rights);
