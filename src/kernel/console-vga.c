afb17e9ee8aee63a838d374df8e4f6ad7c865a7d
Gwen Nelson <gwen@gwennelson.co.uk>
2026-09-10 20:55:01 +0100

    have VGA driver service an IPC endpoint

--- src/kernel/console-vga.c
+++ src/kernel/console-vga.c
@@ -7,7 +7,10 @@
 #include <sharkix/kernel/sync.h>
 #include <sharkix/kernel/kvalloc.h>
 #include <sharkix/kernel/caps.h>
+#include <sharkix/kernel/ipc.h>
+#include <sharkix/kernel/syscall.h>
 #include <sharkix/kernel/thread.h>
+#include <sharkix/libsharkix/syscalls.h>
 
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 25
@@ -23,13 +26,43 @@ static uint8_t vga_x, vga_y;
 static pmem_handle_t vga_pmem = PMEM_INVALID_HANDLE;
 static cap_handle_t vga_cap = CAP_INVALID_HANDLE;
 static capset_handle_t vga_server_capset;
+static cap_handle_t vga_endpoint_cap = CAP_INVALID_HANDLE;
 static thread_t *vga_server_thread;
 static bool vga_ready;
 
 static void console_vga_server_thread(void *argument)
 {
-    (void)argument;
-    for (;;) thread_yield();
+    capset_handle_t capset = (capset_handle_t)(uintptr_t)argument;
+    cap_t pmem_cap;
+    cap_t endpoint_cap;
+    sharkix_syscall_regs_t regs = { 0 };
+
+    if (kcapset_resolve_cap(capset, CAP_TYPE_PMEM,
+                            CAP_RIGHT_PMEM_MAP | CAP_RIGHT_PMEM_READ |
+                            CAP_RIGHT_PMEM_WRITE, &pmem_cap) != 0 ||
+        kcapset_resolve_cap(capset, CAP_TYPE_IPC_ENDPOINT,
+                            CAP_RIGHT_IPC_RECV, &endpoint_cap) != 0)
+        for (;;) thread_yield();
+
+    (void)pmem_cap;
+    regs.rax = SYSCALL_IPC_RECV;
+    regs.rdi = endpoint_cap.cap_handle;
+    for (;;) {
+        uint64_t words[4];
+        uint64_t count;
+
+        sharkix_syscall(&regs);
+        count = regs.rsi;
+        if (count > 32) count = 32;
+        words[0] = regs.rdx;
+        words[1] = regs.r10;
+        words[2] = regs.r8;
+        words[3] = regs.r9;
+        for (uint64_t i = 0; i < count; ++i)
+            console_vga_putc((char)(words[i / 8] >> ((i % 8) * 8)));
+        regs.rax = SYSCALL_IPC_RECV;
+        regs.rdi = endpoint_cap.cap_handle;
+    }
 }
 
 static void outb(uint16_t port, uint8_t value) { __asm__ volatile ("outb %0, %1" : : "a"(value), "Nd"(port)); }
@@ -76,9 +109,15 @@ void console_vga_init(void) {
 
      // for now, we create the vga-consoled thread in ring0
 
-     if (kcapset_new(&vga_server_capset) != 0 ||
-         kcapset_addcap(vga_server_capset, vga_cap) != 0) {
-         console_write("console-vga.c:console_vga_init() - failed to create VRAM server capset!\n");
+     ipc_handle_t endpoint;
+     if (ipc_create(&endpoint) != IPC_OK ||
+         kcap_create((kobject_handle_t)endpoint, CAP_TYPE_IPC_ENDPOINT,
+                     CAP_RIGHT_IPC_RECV, &vga_endpoint_cap) != 0 ||
+         kcapset_new(&vga_server_capset) != 0 ||
+         kcapset_addcap(vga_server_capset, vga_cap) != 0 ||
+         kcapset_addcap(vga_server_capset, vga_endpoint_cap) != 0 ||
+         kcapset_addcap(address_space_kernel()->capset, vga_endpoint_cap) != 0) {
+         console_write("console-vga.c:console_vga_init() - failed to setup server IPC!\n");
          return;
      }
 
@@ -87,7 +126,7 @@ void console_vga_init(void) {
          .kernel_stack_size = 64 * PAGE_SIZE,
          .name = "vga-consoled",
          .priority = tskIDLE_PRIORITY + 1,
-         .argument = &vga_server_capset
+         .argument = (void *)(uintptr_t)vga_server_capset
      };
      vga_server_thread = thread_create(address_space_kernel(),
                                        THREAD_PRIVILEGE_KERNEL,
