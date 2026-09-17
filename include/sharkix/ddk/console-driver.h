13030ed1ec94eac357632ac229cf5af14a1ee33d
Gwen Nelson <gwen@gwennelson.co.uk>
2026-09-17 15:19:16 +0100

    ALIGN YOUR STRUCTS, BITCHES

--- include/sharkix/ddk/console-driver.h
+++ include/sharkix/ddk/console-driver.h
@@ -2,33 +2,51 @@
 
 #include <stdbool.h>
 
-// this struct needs to be defined somewhere in every console driver within a .console_drivers section
-// to make it easier, use the macros below
+/*
+ * Console driver descriptor.
+ *
+ * Console driver linker sets are treated as contiguous arrays of these
+ * structures, so registrations must use the natural alignment of this type.
+ */
 typedef struct sharkix_console_driver_t {
-      const char *name;
-      void (*init)(void);
-      bool (*ready)(void);
-      void  (*putc)(char c);
+    const char *name;
+    void (*init)(void);
+    bool (*ready)(void);
+    void (*putc)(char c);
 } sharkix_console_driver_t;
 
 
-// this macro goes at global level of any .c file
-#define REGISTER_CONSOLE_DRIVER(driver_name, init_fn, ready_fn, putc_fn) \
-	static sharkix_console_driver_t __attribute__((section(".console_drivers"))) __attribute__((used)) \
-	_console_##driver_name = { \
-		.name  = #driver_name, \
-		.init  = init_fn, \
-		.ready = ready_fn, \
-		.putc  = putc_fn, \
-	}
+/*
+ * Register a normal console driver.
+ *
+ * This macro must be used at global scope.
+ */
+#define REGISTER_CONSOLE_DRIVER(driver_name, init_fn, ready_fn, putc_fn)       \
+    static sharkix_console_driver_t                                            \
+        __attribute__((section(".console_drivers")))                           \
+        __attribute__((used))                                                  \
+        __attribute__((aligned(__alignof__(sharkix_console_driver_t))))        \
+        _console_##driver_name = {                                             \
+            .name  = #driver_name,                                             \
+            .init  = init_fn,                                                  \
+            .ready = ready_fn,                                                 \
+            .putc  = putc_fn,                                                  \
+        }
 
+
+/*
+ * Register an early console driver.
+ *
+ * This macro must be used at global scope.
+ */
 #define REGISTER_EARLY_CONSOLE_DRIVER(driver_name, init_fn, ready_fn, putc_fn) \
-	static sharkix_console_driver_t __attribute__((section(".console_early_drivers"))) __attribute__((used)) \
-	_console_##driver_name = { \
-		.name  = #driver_name, \
-		.init  = init_fn, \
-		.ready = ready_fn, \
-		.putc  = putc_fn, \
-	}
-
-
+    static sharkix_console_driver_t                                            \
+        __attribute__((section(".console_early_drivers")))                     \
+        __attribute__((used))                                                  \
+        __attribute__((aligned(__alignof__(sharkix_console_driver_t))))        \
+        _console_##driver_name = {                                             \
+            .name  = #driver_name,                                             \
+            .init  = init_fn,                                                  \
+            .ready = ready_fn,                                                 \
+            .putc  = putc_fn,                                                  \
+        }
