ebdfee6c31e8858b01d45b1494bcf1cd54002603
Gwen Nelson <gwen@gwennelson.co.uk>
2026-09-17 16:01:41 +0100

    begin cleaning up DDK stuff

--- src/drivers/module.mk
+++ src/drivers/module.mk
@@ -1,6 +1,9 @@
 DRIVER_MODULE_ROOT := $(DRIVER_SRC_ROOT)
 DRIVER_SRCS :=
 
+DRIVER_COMMON_ROOT := $(DRIVER_MODULE_ROOT)/common
+
+DRIVER_RING3_LINKER := $(DRIVER_COMMON_ROOT)/ring3.ld
 
 $(foreach driver,$(PLATFORM_DRIVERS),\
       $(eval $(call include-selection,$(DRIVER_SRC_ROOT)/$(driver)/module.mk)))
